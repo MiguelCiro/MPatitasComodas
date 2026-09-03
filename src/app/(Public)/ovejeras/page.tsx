9a31464fe4cfc2cf67a0346102099439da514088
Miguel Angel Ciro Garcia <miguelangelcirogarcia7@gmail.com>
2026-09-02 20:38:14 -0500

    fix: refresh public product categories

--- src/app/(Public)/ovejeras/page.tsx
+++ src/app/(Public)/ovejeras/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 import ProductList from "@/sections/ProductList/ProductList";
 
 export default function OvejerasPage() {
--- src/app/(Public)/personajes/page.tsx
+++ src/app/(Public)/personajes/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 import ProductList from "@/sections/ProductList/ProductList";
 
 export default function PersonajesPage() {
